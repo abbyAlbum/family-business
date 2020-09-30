e8bb9fa7a47e450b5d1bca147599542d2ef37baa
Abby Album <a.album@hotmail.co.uk>
2020-09-30 11:23:48 +0300

    Calendar

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -12,9 +12,21 @@ import {
 import Calendar from "react-calendar";
 
 class calendar extends React.Component {
+  data = [
+    {
+      Id: 2,
+      Subject: "Paris",
+      StartTime: new Date(2020, 8, 15, 10, 0),
+      EndTime: new Date(2020, 8, 15, 12, 30),
+    },
+  ];
+
   render() {
     return (
-      <ScheduleComponent currentView="Month">
+      <ScheduleComponent
+        currentView="Month"
+        eventSettings={{ dataSource: this.data }}
+      >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
       </ScheduleComponent>
     );
