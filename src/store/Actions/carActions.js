f8974122ee5c31f22b1f138d3943910e8874355b
Abby Album <a.album@hotmail.co.uk>
2020-10-22 15:02:11 +0300

    get days from firestore

--- src/store/Actions/carActions.js
+++ src/store/Actions/carActions.js
@@ -1,18 +1,45 @@
-export const addCarDay = (day, name, time) => {
+var i = true;
+
+export const addCarDay = (day, name) => {
   return (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore();
-    firestore
-      .collection("car")
-      .add({
-        day: day,
-        name: name,
-        time: time,
-      })
-      .then(() => {
-        dispatch({ type: "ADD_CAR_DAY_SUCCESS" });
-      })
-      .catch((err) => {
-        dispatch({ type: "ADD_CAR_DAY_ERROR", err });
-      });
+    fetchDay(day, firestore);
+    if (i) {
+      firestore
+        .collection("car")
+        .add({
+          day: day,
+          name: name,
+          time: "",
+        })
+        .then(() => {
+          dispatch({ type: "ADD_CAR_DAY_SUCCESS" });
+        })
+        .catch((err) => {
+          dispatch({ type: "ADD_CAR_DAY_ERROR", err });
+        });
+    }
   };
 };
+
+async function fetchDay(days, firestore) {
+  firestore
+    .collection("car")
+    .get()
+    .then((querySnapshot) => {
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data().day);
+          if (days === doc.data().day) {
+            console.log(days);
+            var i = false;
+
+            console.log(i);
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
