f221e24ab0d3b00631013c3f547182f2a4444fc9
erm19 <eyal.moskowitz@gmail.com>
2020-10-25 08:24:06 +0200

    fix waiting problem in car

--- src/store/Actions/carActions.js
+++ src/store/Actions/carActions.js
@@ -1,45 +1,37 @@
-var i = true;
+//var i = true;
 
 export const addCarDay = (day, name) => {
-  return (dispatch, getState, { getFirestore }) => {
+  return async (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore();
-    fetchDay(day, firestore);
+    var i = await fetchDay(day, firestore);
+    console.log("we're in add " + i + " after fetch")
     if (i) {
-      firestore
-        .collection("car")
-        .add({
+      firestore.collection("car").add({
           day: day,
           name: name,
           time: "",
-        })
-        .then(() => {
+        }).then(() => {
           dispatch({ type: "ADD_CAR_DAY_SUCCESS" });
-        })
-        .catch((err) => {
+        }).catch((err) => {
           dispatch({ type: "ADD_CAR_DAY_ERROR", err });
         });
     }
   };
 };
 
-async function fetchDay(days, firestore) {
-  firestore
-    .collection("car")
-    .get()
-    .then((querySnapshot) => {
-      if (!querySnapshot.empty) {
-        querySnapshot.forEach((doc) => {
-          console.log(doc.data().day);
-          if (days === doc.data().day) {
-            console.log(days);
-            var i = false;
-
-            console.log(i);
-          }
-        });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const fetchDay = async (days, firestore) => {
+  var flag = await firestore.collection("car").get().then((querySnapshot) => {
+    if (!querySnapshot.empty) {
+      var flag = true
+      querySnapshot.forEach((doc) => {
+        if (days === doc.data().day)
+          flag = false;
+      });
+      return flag;
+    }
+  }).catch((error) => {
+    console.log(error);
+  });
+  console.log(flag);
+  return flag;
 }
